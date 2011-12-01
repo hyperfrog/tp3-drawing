919e582c46043636aa6a0475a9d1e61ec2029cd6
lesage.christian@gmail.com <lesage.christian@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-01 06:36:49 +0000

    Le polygone peut maintenant être déplacé avec Shift après avoir été sélectionné. En mode création de polygone, un point est ajouté dynamiquement au polygone en suivant le déplacement de la souris.

--- Drawing/src/appDrawing/model/VPolygon.java
+++ Drawing/src/appDrawing/model/VPolygon.java
@@ -15,65 +15,66 @@ public class VPolygon extends Shape
 	//liste de points du polygone
 	ArrayList<Point2D> points = new ArrayList<Point2D>();
 	
-	//Initialisation du polygone et ajout du premier point
-	public VPolygon(float posX, float posY, float width, float height)
-	{
-		super(posX, posY, width, height);
-		this.points.add(new Point2D.Float(posX,posY));
-		
-	}
 	/**
-	 * Ajoute un point(En coordonées virtuelles) dans le polygone.
+	 * Construit un polygone à partir d'un point de départ.
+	 * 
+	 * @param posX Coordonnée virtuelle du premier point en x 
+	 * @param posY Coordonnée virtuelle du premier point en y 
+	 */
+	public VPolygon(float posX, float posY)
+	{
+		super(posX, posY, 0, 0);
+		this.points.add(new Point2D.Float(posX, posY));
+	}
+
+	/**
+	 * Ajoute au polygone un point spécifié en coordonnées réelles.
+	 * 
 	 * @param realX	Coordonée réelle du point en X (Au clic de la souris)
 	 * @param realY Coordonée réelle du point en Y (Au clic de la souris)
 	 * @param scalingFactor scalingFactor du DrawingPanel
 	 * @param virtualDeltaX virtualDeltaX du DrawingPanel
 	 * @param virtualDeltaY virtualDeltaY du DrawingPanel
 	 */
-	public void addPoint(float realX,float realY,float scalingFactor,float virtualDeltaX,float virtualDeltaY)
+	public void addPoint(int realX, int realY, float scalingFactor, float virtualDeltaX, float virtualDeltaY)
 	{
-		// Calcule les coordonnées et dimensions virtuelles
-		float virtualX = (realX / scalingFactor) - virtualDeltaX;
-		float virtualY = (realY / scalingFactor) - virtualDeltaY;
+		this.points.add(Shape.getVirtualPoint(realX, realY, scalingFactor, virtualDeltaX, virtualDeltaY));
 		
-		//ajout des coordonnées virtuelles calculées
-		this.points.add(new Point2D.Float(virtualX, virtualY));
-		
-		//update la hauteur et largeur de la forme
-		double maxX = this.points.get(0).getX();
-		double minX = this.points.get(0).getX();
-		double maxY = this.points.get(0).getY();
-		double minY = this.points.get(0).getY();
-		
-		for(int i = 1;i < this.points.size();++i)
+		this.computeDimensions();
+	}
+
+	protected void computeDimensions()
+	{
+		if (this.points != null && this.points.size() > 0)
 		{
-			//Pour la largeur
-			if(this.points.get(i).getX() > maxX)
+			//update la hauteur et largeur de la forme
+			double maxX = this.points.get(0).getX();
+			double minX = this.points.get(0).getX();
+			double maxY = this.points.get(0).getY();
+			double minY = this.points.get(0).getY();
+
+			for(int i = 1; i < this.points.size(); ++i)
 			{
-				maxX = this.points.get(i).getX();
+				//Pour la largeur
+				maxX = Math.max(maxX, this.points.get(i).getX());
+				minX = Math.min(minX, this.points.get(i).getX());
+				//Pour la hauteur
+				maxY = Math.max(maxY, this.points.get(i).getY());
+				minY = Math.min(minY, this.points.get(i).getY());
 			}
-			if(this.points.get(i).getX() < minX)
-			{
-				minX = this.points.get(i).getX();
-			}
-			//Pour la hauteur
-			if(this.points.get(i).getY() > maxY)
-			{
-				maxY = this.points.get(i).getY();
-			}
-			if(this.points.get(i).getY() < minY)
-			{
-				minY = this.points.get(i).getY();
-			}
+
+			//calcul de la largeur et la hauteur
+			this.width = (float) (maxX - minX);
+			this.height = (float) (maxY - minY);
+			//on définit comme point de depart le point en haut à gauche (comme dans un rectangle)
+			this.posX = (float) minX;
+			this.posY = (float) minY;
 		}
-		
-		//calcul de la largeur et la hauteur
-		this.width = (float) (maxX - minX);
-		this.height = (float) (maxY - minY);
-		//on définit comme point de depart le point en haut à gauche (comme dans un rectangle)
-		this.posX = (float) minX;
-		this.posY = (float) minY;
-		
+		else
+		{
+			this.width = 0;
+			this.height = 0;
+		}
 	}
 	
 	/* (non-Javadoc)
@@ -84,18 +85,73 @@ public class VPolygon extends Shape
 	{
 		// Obtient les coordonnées réelles du rectangle englobant
 	       
-		int[] xPoints = new int[points.size()];
-		int[] yPoints = new int[points.size()];
+		int[] xPoints = new int[this.points.size()];
+		int[] yPoints = new int[this.points.size()];
 		
-		for(int i = 0; i < points.size(); ++i)
+		for(int i = 0; i < this.points.size(); ++i)
 		{
-			xPoints[i] = (int) Math.round( ( drawingDeltaX + points.get(i).getX() ) * drawingScalingFactor );
-			yPoints[i] = (int) Math.round( ( drawingDeltaY + points.get(i).getY() ) * drawingScalingFactor );
+			xPoints[i] = (int) Math.round( ( drawingDeltaX + this.points.get(i).getX() ) * drawingScalingFactor * this.scalingFactor);
+			yPoints[i] = (int) Math.round( ( drawingDeltaY + this.points.get(i).getY() ) * drawingScalingFactor * this.scalingFactor);
 		}
 		
-		Polygon model = new Polygon(xPoints, yPoints, points.size());
+		Polygon model = new Polygon(xPoints, yPoints, this.points.size());
 		
 		this.drawShape(g, model, drawingScalingFactor, drawingDeltaX, drawingDeltaY);
 	}
+	
+	/* (non-Javadoc)
+	 * @see appDrawing.model.Shape#setPosition(float, float)
+	 */
+	@Override
+	public void setPosition(float posX, float posY)
+	{
+		// Calcule le déplacement de la forme
+		float deltaX = posX - this.posX; 
+		float deltaY = posY - this.posY; 
+		
+		this.translate(deltaX, deltaY);
+	}
+	
+	/* (non-Javadoc)
+	 * @see appDrawing.model.Shape#move(float, float)
+	 */
+	@Override
+	public void translate(float deltaX, float deltaY)
+	{
+		this.posX += deltaX;
+		this.posY += deltaY;
+		
+		// Déplace chacun des points de la forme
+		ArrayList<Point2D> newPoints = new ArrayList<Point2D>();
+		
+		for(Point2D p : this.points)
+		{
+			newPoints.add(new Point2D.Float((float) p.getX() + deltaX, (float) p.getY() + deltaY));
+		}
+		this.points = newPoints;
+	}
 
+	/**
+	 * Obtient la liste des points du polygone.
+	 * 
+	 * @return liste des points du polygone
+	 */
+	public ArrayList<Point2D> getPoints()
+	{
+		return this.points;
+	}
+	
+	/**
+	 * Affecte une nouvelle liste de points au polygone.
+	 * 
+	 * @param points nouvelle liste de points du polygone
+	 */
+	public void setPoints(ArrayList<Point2D> points)
+	{
+		if (points != null)
+		{
+			this.points = points;
+			this.computeDimensions();
+		}
+	}
 }
