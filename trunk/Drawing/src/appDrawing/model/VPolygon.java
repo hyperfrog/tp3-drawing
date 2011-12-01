b0688f79fb6a4c7f086894db0ea35c5ddd40dfaf
micaellemelin@gmail.com <micaellemelin@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-01 01:45:17 +0000

    Cet update est une excellente nouvelle. En effet, le polygone est maintenant completement fonctionel. Le panning, le zoom, la selection, le dessin de plusieurs polygones et j'en passe.
    Christian, ton bout de code m'a beaucoup aidé à comprendre comment ton système marche. Par contre, il m'a fallu considèrer le polygone comme une "shape" à part.
    
    Si tu as des commentaires, n'hésite pas. Je sais maintenant comment les lire ;)

--- trunk/Drawing/src/appDrawing/model/VPolygon.java
+++ trunk/Drawing/src/appDrawing/model/VPolygon.java
@@ -1,34 +1,79 @@
 package appDrawing.model;
 
 import java.awt.BasicStroke;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class VPolygon extends Shape
 {
-	//Utilise simplement la classe polygone de Graphics2D, mais de manière contrôlée
+	//liste de points du polygone
+	ArrayList<Point2D> points = new ArrayList<Point2D>();
 	
-	/**
-	 * Polygone de la forme
-	 */
-	private Polygon model = null;
-	
-	//Initialisation du polygone, création du premier point
-	public VPolygon(float posX, float posY, float width, float height) {
-		super(posX, posY, width, height);
-	}
-	
-	public void addPoint(int posX,int posY)
+	//Initialisation du polygone et ajout du premier point
+	public VPolygon(float posX, float posY, float width, float height)
 	{
-		if(model == null)
+		super(posX, posY, width, height);
+		this.points.add(new Point2D.Float(posX,posY));
+		
+	}
+	/**
+	 * Ajoute un point(En coordonées virtuelles) dans le polygone.
+	 * @param realX	Coordonée réelle du point en X (Au clic de la souris)
+	 * @param realY Coordonée réelle du point en Y (Au clic de la souris)
+	 * @param scalingFactor scalingFactor du DrawingPanel
+	 * @param virtualDeltaX virtualDeltaX du DrawingPanel
+	 * @param virtualDeltaY virtualDeltaY du DrawingPanel
+	 */
+	public void addPoint(float realX,float realY,float scalingFactor,float virtualDeltaX,float virtualDeltaY)
+	{
+		// Calcule les coordonnées et dimensions virtuelles
+		float virtualX = (realX / scalingFactor) - virtualDeltaX;
+		float virtualY = (realY / scalingFactor) - virtualDeltaY;
+		
+		//ajout des coordonnées virtuelles calculées
+		this.points.add(new Point2D.Float(virtualX, virtualY));
+		
+		//update la hauteur et largeur de la forme
+		double maxX = this.points.get(0).getX();
+		double minX = this.points.get(0).getX();
+		double maxY = this.points.get(0).getY();
+		double minY = this.points.get(0).getY();
+		
+		for(int i = 1;i < this.points.size();++i)
 		{
-			model = new Polygon();
+			//Pour la largeur
+			if(this.points.get(i).getX() > maxX)
+			{
+				maxX = this.points.get(i).getX();
+			}
+			if(this.points.get(i).getX() < minX)
+			{
+				minX = this.points.get(i).getX();
+			}
+			//Pour la hauteur
+			if(this.points.get(i).getY() > maxY)
+			{
+				maxY = this.points.get(i).getY();
+			}
+			if(this.points.get(i).getY() < minY)
+			{
+				minY = this.points.get(i).getY();
+			}
 		}
 		
-		model.addPoint(posX,posY);
+		//calcul de la largeur et la hauteur
+		this.width = (float) (maxX - minX);
+		this.height = (float) (maxY - minY);
+		//on définit comme point de depart le point en haut à gauche (comme dans un rectangle)
+		this.posX = (float) minX;
+		this.posY = (float) minY;
+		
 	}
 	
 	/* (non-Javadoc)
@@ -38,16 +83,19 @@ public class VPolygon extends Shape
 	public void draw(Graphics2D g, float drawingScalingFactor, float drawingDeltaX, float drawingDeltaY)
 	{
 		// Obtient les coordonnées réelles du rectangle englobant
-		//Rectangle r = this.getRealRect(drawingScalingFactor, drawingDeltaX, drawingDeltaY);
-		g.setPaint(this.getGradientPaint(drawingScalingFactor, drawingDeltaX, drawingDeltaY));
-		g.fill(model);
-		g.setColor(this.strokeColor);
-		g.setStroke(new BasicStroke(
-				this.strokeWidth * drawingScalingFactor * this.scalingFactor, 
-				BasicStroke.CAP_ROUND, 
-				BasicStroke.JOIN_ROUND));
-		g.draw(model);
+	       
+		int[] xPoints = new int[points.size()];
+		int[] yPoints = new int[points.size()];
 		
+		for(int i = 0; i < points.size(); ++i)
+		{
+			xPoints[i] = (int) Math.round( ( drawingDeltaX + points.get(i).getX() ) * drawingScalingFactor );
+			yPoints[i] = (int) Math.round( ( drawingDeltaY + points.get(i).getY() ) * drawingScalingFactor );
+		}
+		
+		Polygon model = new Polygon(xPoints, yPoints, points.size());
+		
+		this.drawShape(g, model, drawingScalingFactor, drawingDeltaX, drawingDeltaY);
 	}
 
 }
