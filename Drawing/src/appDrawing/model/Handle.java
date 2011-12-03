6ffab1906628e4c7a5022a30a55616ad36a32f6a
lesage.christian@gmail.com <lesage.christian@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-03 07:49:07 +0000

    Optimisations dans Handle et Shape.

--- Drawing/src/appDrawing/model/Handle.java
+++ Drawing/src/appDrawing/model/Handle.java
@@ -4,11 +4,11 @@ import java.awt.BasicStroke;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 
-public class Handle extends Shape{
-
-	private static final int handleSize = 6;//6 pixels de large
+public class Handle extends Shape
+{
+	private static final int HANDLE_SIZE = 6;//6 pixels de large
 	
-	public static enum handleType
+	public static enum HandleType
 	{
 		TOP_LEFT		(0,   0   ),//point d'origine gauche
 		TOP_MIDDLE		(0.5f,0   ),//^
@@ -17,35 +17,41 @@ public class Handle extends Shape{
 		MIDDLE_RIGHT	(1,   0.5f),//|
 		BOTTOM_LEFT		(0,   1   ),//|
 		BOTTOM_MIDDLE	(0.5f,1   ),//v
-		BOTTOM_RIGHT	(1,   1   );//point en bas à gauche
+		BOTTOM_RIGHT	(1,   1   );//point en bas à droite
 		
-		public float xModifer;
-		public float yModifer;
+		public final float xModifier;
+		public final float yModifier;
 		
-		private handleType(float xModif,float yModif) {
-			this.xModifer = xModif;
-			this.yModifer = yModif;
+		private HandleType(float xModif, float yModif)
+		{
+			this.xModifier = xModif;
+			this.yModifier = yModif;
 		}
-		
 	};
+
+	private Shape parent = null;
+	private HandleType type;
 	
-	public Handle(handleType type, float shapePosX, float shapePosY, float shapeWidth, float shapeHeight)
+	public Handle(HandleType type, Shape shape)
 	{	
-		super(shapePosX, shapePosY, Handle.handleSize, Handle.handleSize);
-		
-		this.setPosition(shapePosX+(type.xModifer * shapeWidth), shapePosY+(type.yModifer * shapeHeight));
+		super(shape.getPosX(), shape.getPosY(), Handle.HANDLE_SIZE, Handle.HANDLE_SIZE);
+		this.type = type;
+		this.parent = shape;
 	}
 
 	@Override
 	public void draw(Graphics2D g, float drawingScalingFactor, float drawingDeltaX, float drawingDeltaY) 
 	{
-		java.awt.Rectangle r = getRealRect(drawingScalingFactor, drawingDeltaX, drawingDeltaY);
+		this.setPosition(
+				this.parent.getPosX() + (this.type.xModifier * this.parent.getWidth()), 
+				this.parent.getPosY() + (this.type.yModifier * this.parent.getHeight()));
+
+		java.awt.Rectangle r = this.getRealRect(drawingScalingFactor, drawingDeltaX, drawingDeltaY);
 		
 		g.setColor(Shape.SELECTION_COLOR);
 		g.setStroke(new BasicStroke(this.width));
 		
 		this.drawPoint(g, r.x, r.y);
-		
 		
 	}
 
