2a868d1d06f2eeaeafdb468ad87d395c903cf249
turcot4255@gmail.com <turcot4255@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-06 19:43:52 +0000

    création de ShapeListBar, la classe n'est pas terminé

---
+++ trunk/Drawing/src/appDrawing/form/AppShapeListBar.java
@@ -0,0 +1,84 @@
+package appDrawing.form;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+import appDrawing.model.Shape;
+
+public class AppShapeListBar extends JPanel implements ListDataListener
+{
+	private int shapeCount;
+	private JList bigDataList;
+	private ArrayList<Shape> shapeList;
+	private Board parent = null;
+	DefaultListModel bigData = new DefaultListModel();
+	
+	
+	public AppShapeListBar(Board parent)
+	{
+		this.parent = parent;
+		
+		shapeList = parent.getDrawingPanel().getShapeList();
+		shapeCount = 10;
+		
+		bigDataList = new JList(bigData);
+		bigDataList.setPrototypeCellValue("Index 1234567890");
+		JScrollPane scrollPane = new JScrollPane(bigDataList);
+		this.setLayout(new BorderLayout());
+		this.add(scrollPane, BorderLayout.CENTER);
+		
+
+		 
+	}
+	
+	//DefaultListModel bigData = new DefaultListModel() {
+	//     public int getSize() { return shapeCount; }
+	//     public Object getElementAt(int index) { return "bla"; }
+	//};
+
+	public void testDynamicList(ArrayList<Shape> list)
+	{
+		bigData.removeAllElements();
+		for (int i = 0; i < list.size(); i++)
+		{
+			bigData.addElement(list.get(i).getName());
+		}
+		
+		
+		
+	}
+
+	@Override
+	public void contentsChanged(ListDataEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void intervalAdded(ListDataEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void intervalRemoved(ListDataEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
