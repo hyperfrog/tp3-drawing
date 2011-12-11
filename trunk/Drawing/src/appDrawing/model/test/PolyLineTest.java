81995d2698d37b42539e73a0caeb5d6952c36b0d
micaellemelin@gmail.com <micaellemelin@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-11 00:54:19 +0000

    Tests faits pour le polyline. Pareil au polygone...

--- trunk/Drawing/src/appDrawing/model/test/PolyLineTest.java
+++ trunk/Drawing/src/appDrawing/model/test/PolyLineTest.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import appDrawing.model.PolyLine;
+import appDrawing.model.Polygon;
+
 public class PolyLineTest {
 
 	@Test
-	public void testPolyLine() {
-		fail("Not yet implemented");
+	public void testPolyLine()
+	{	
+		float startX = 0;
+		float startY = 0;
+		
+		PolyLine poly = new PolyLine(startX, startY);
+		
+		assertNotNull(poly);
+		
 	}
 
 }
