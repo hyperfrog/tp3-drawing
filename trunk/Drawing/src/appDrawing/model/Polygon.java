1996dd58ee858bf463f1b78033774da6784104ac
lesage.christian@gmail.com <lesage.christian@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-11 21:58:24 +0000

    Mise en forme + exécution au démarrage des classes de tests implémentées (il faudra ajouter les autres dans MainApp quand elles seront implémentées).

--- trunk/Drawing/src/appDrawing/model/Polygon.java
+++ trunk/Drawing/src/appDrawing/model/Polygon.java
@@ -5,29 +5,29 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 /**
- * Classe qui représente un polygone. En fait, la classe contient une liste de Point2D.
- * Ainsi, lorsque la forme se dessine, elle crée un nouveau java.awt.Polygon et le dessine
- * dans le graphics.
+ * Classe qui représente un polygone. En fait, la classe contient une liste de
+ * Point2D. Ainsi, lorsque la forme se dessine, elle crée un nouveau
+ * java.awt.Polygon et le dessine dans le graphics.
  * 
  * @author Micaël Lemelin
  * @author Christian Lesage
  * @author Alexandre Tremblay
  * @author Pascal Turcot
  * 
- * @see	Point2D
- * @see	java.awt.Polygon
- *
+ * @see Point2D
+ * @see java.awt.Polygon
+ * 
  */
 public class Polygon extends Shape
 {
-	//liste de points du polygone
+	// liste de points du polygone
 	protected ArrayList<Point2D> points = new ArrayList<Point2D>();
-	
+
 	/**
 	 * Construit un polygone à partir d'un point de départ.
 	 * 
-	 * @param posX Coordonnée virtuelle du premier point en x 
-	 * @param posY Coordonnée virtuelle du premier point en y 
+	 * @param posX Coordonnée virtuelle du premier point en x
+	 * @param posY Coordonnée virtuelle du premier point en y
 	 */
 	public Polygon(float posX, float posY)
 	{
@@ -36,32 +36,33 @@ public class Polygon extends Shape
 		this.computeDimensions();
 	}
 
-	// Recalcule les dimensions du polygone après un déplacement, un agrandissement
-	// ou tout changement apporté à la liste de points
+	// Recalcule les dimensions du polygone après un déplacement, un
+	// agrandissement ou tout changement apporté à la liste de points
 	protected void computeDimensions()
 	{
 		if (this.points != null && this.points.size() > 0)
 		{
-			//update la hauteur et largeur de la forme
+			// update la hauteur et largeur de la forme
 			double maxX = this.points.get(0).getX();
 			double minX = this.points.get(0).getX();
 			double maxY = this.points.get(0).getY();
 			double minY = this.points.get(0).getY();
 
-			for(int i = 1; i < this.points.size(); ++i)
+			for (int i = 1; i < this.points.size(); ++i)
 			{
-				//Pour la largeur
+				// Pour la largeur
 				maxX = Math.max(maxX, this.points.get(i).getX());
 				minX = Math.min(minX, this.points.get(i).getX());
-				//Pour la hauteur
+				// Pour la hauteur
 				maxY = Math.max(maxY, this.points.get(i).getY());
 				minY = Math.min(minY, this.points.get(i).getY());
 			}
 
-			//calcul de la largeur et la hauteur
+			// calcul de la largeur et la hauteur
 			this.width = Math.max(Float.MIN_VALUE, (float) (maxX - minX));
 			this.height = Math.max(Float.MIN_VALUE, (float) (maxY - minY));
-			//on définit comme point de depart le point en haut à gauche (comme dans un rectangle)
+			// on définit comme point de départ le point en haut à gauche (comme
+			// dans un rectangle)
 			this.posX = (float) minX;
 			this.posY = (float) minY;
 		}
@@ -72,8 +73,10 @@ public class Polygon extends Shape
 		}
 		this.createHandles();
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see appDrawing.model.Shape#redraw(java.awt.Graphics2D, float)
 	 */
 	@Override
@@ -81,19 +84,21 @@ public class Polygon extends Shape
 	{
 		int[] xPoints = new int[this.points.size()];
 		int[] yPoints = new int[this.points.size()];
-		
-		for(int i = 0; i < this.points.size(); ++i)
+
+		for (int i = 0; i < this.points.size(); ++i)
 		{
-			xPoints[i] = (int) Math.round( ( drawingDeltaX + this.points.get(i).getX() ) * drawingScalingFactor );
-			yPoints[i] = (int) Math.round( ( drawingDeltaY + this.points.get(i).getY() ) * drawingScalingFactor );
+			xPoints[i] = (int) Math.round((drawingDeltaX + this.points.get(i).getX()) * drawingScalingFactor);
+			yPoints[i] = (int) Math.round((drawingDeltaY + this.points.get(i).getY()) * drawingScalingFactor);
 		}
-		
+
 		java.awt.Polygon model = new java.awt.Polygon(xPoints, yPoints, this.points.size());
-		
+
 		this.drawShape(g, model, drawingScalingFactor, drawingDeltaX, drawingDeltaY, true);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see appDrawing.model.Shape#move(float, float)
 	 */
 	@Override
@@ -101,17 +106,19 @@ public class Polygon extends Shape
 	{
 		// Déplace chacun des points de la forme
 		ArrayList<Point2D> newPoints = new ArrayList<Point2D>();
-		
-		for(Point2D p : this.points)
+
+		for (Point2D p : this.points)
 		{
 			newPoints.add(new Point2D.Float((float) p.getX() + deltaX, (float) p.getY() + deltaY));
 		}
-		
+
 		this.points = newPoints;
 		super.translate(deltaX, deltaY);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see appDrawing.model.Shape#scaleWidth(float)
 	 */
 	@Override
@@ -120,21 +127,23 @@ public class Polygon extends Shape
 		if (scalingFactor > 0 && scalingFactor != 1)
 		{
 			ArrayList<Point2D> newPoints = new ArrayList<Point2D>();
-			
-			// Transforme chacun des points 
+
+			// Transforme chacun des points
 			for (Point2D p : this.points)
 			{
 				float deltaX = (float) (p.getX() - refX);
 				float newDeltaX = deltaX * scalingFactor;
 				newPoints.add(new Point2D.Float(refX + newDeltaX, (float) p.getY()));
 			}
-			
+
 			this.points = newPoints;
 			super.scaleWidth(scalingFactor, refX);
 		}
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see appDrawing.model.Shape#scaleHeight(float)
 	 */
 	@Override
@@ -143,20 +152,20 @@ public class Polygon extends Shape
 		if (scalingFactor > 0 && scalingFactor != 1)
 		{
 			ArrayList<Point2D> newPoints = new ArrayList<Point2D>();
-			
-			// Transforme chacun des points 
+
+			// Transforme chacun des points
 			for (Point2D p : this.points)
 			{
 				float deltaY = (float) (p.getY() - refY);
 				float newDeltaY = deltaY * scalingFactor;
 				newPoints.add(new Point2D.Float((float) p.getX(), refY + newDeltaY));
 			}
-			
+
 			this.points = newPoints;
 			super.scaleHeight(scalingFactor, refY);
 		}
 	}
-	
+
 	/**
 	 * Retourne la liste des points du polygone.
 	 * 
@@ -166,7 +175,7 @@ public class Polygon extends Shape
 	{
 		return this.points;
 	}
-	
+
 	/**
 	 * Affecte une nouvelle liste de points au polygone.
 	 * 
--- trunk/Drawing/src/appDrawing/model/test/PolyLineTest.java
+++ trunk/Drawing/src/appDrawing/model/test/PolyLineTest.java
@@ -5,20 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import appDrawing.model.PolyLine;
-import appDrawing.model.Polygon;
 
-public class PolyLineTest {
-
+public class PolyLineTest
+{
 	@Test
 	public void testPolyLine()
-	{	
+	{
 		float startX = 0;
 		float startY = 0;
-		
-		PolyLine poly = new PolyLine(startX, startY);
-		
-		assertNotNull(poly);
-		
-	}
 
+		PolyLine poly = new PolyLine(startX, startY);
+
+		assertNotNull(poly);
+	}
 }
