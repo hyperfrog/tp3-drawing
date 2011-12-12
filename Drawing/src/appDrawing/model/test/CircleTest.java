6a7d114c7dc7dc2bee15980363f8ef89801282e8
micaellemelin@gmail.com <micaellemelin@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-12 00:18:30 +0000

    Test Ellipse et amélioration des test du Cercle

--- Drawing/src/appDrawing/model/test/CircleTest.java
+++ Drawing/src/appDrawing/model/test/CircleTest.java
@@ -14,7 +14,7 @@ public class CircleTest {
 		//Pour les tests de validitée des valeurs d'entrées voir appDrawing.model.test.ShapeTest
 		
 		//Test du constructeur du cercle, la plus petite valeur de dimension est prise pour le diamètre
-		Circle c = new Circle(0.0f, 0.0f, 10.0f, 2.0f);
+		Circle c = new Circle(2.0f, 1.0f, 10.0f, 2.0f);
 		
 		//D'abord vérifions que le cercle est créé
 		assertNotNull(c);
@@ -23,12 +23,20 @@ public class CircleTest {
 		assertEquals(2.0f, c.getWidth(),0);
 		assertEquals(2.0f, c.getHeight(),0);
 		
+		//Vérification des autres paramètres
+		assertEquals(2.0f, c.getPosX(),0);
+		assertEquals(1.0f, c.getPosY(),0);
+				
 		//À l'inverse, si la hauteur est plus grande la largeur doit être le diamètre
-		c = new Circle(0.0f, 0.0f, 2.0f, 10.0f);
+		c = new Circle(2.0f, 1.0f, 2.0f, 10.0f);
 				
 		//Ici le cercle doit avoir un diamètre de 2.0f encore
 		assertEquals(2.0f, c.getWidth(),0);
 		assertEquals(2.0f, c.getHeight(),0);
+		
+		//Vérification des autres paramètres
+		assertEquals(2.0f, c.getPosX(),0);
+		assertEquals(1.0f, c.getPosY(),0);
 	}
 
 	@Test
@@ -37,7 +45,7 @@ public class CircleTest {
 		//Pour les tests de validitée des valeurs d'entrées voir appDrawing.model.test.ShapeTest
 		
 		//Test du constructeur du cercle qui prend un diamètre en paramètre
-		Circle c = new Circle(0.0f, 0.0f, 10.0f);
+		Circle c = new Circle(3.0f, 4.0f, 10.0f);
 				
 		//D'abord vérifions que le cercle est créé
 		assertNotNull(c);
@@ -45,6 +53,10 @@ public class CircleTest {
 		//Ici le cercle doit avoir une hauteur de 10.0f et une largeur égale
 		assertEquals(10.0f, c.getWidth(),0);
 		assertEquals(10.0f, c.getHeight(),0);
+		
+		//Vérification des autres paramètres
+		assertEquals(3.0f, c.getPosX(),0);
+		assertEquals(4.0f, c.getPosY(),0);
 	}
 
 }
--- Drawing/src/appDrawing/model/test/EllipseTest.java
+++ Drawing/src/appDrawing/model/test/EllipseTest.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import appDrawing.model.Ellipse;
+
 public class EllipseTest {
 
 	@Test
-	public void testEllipse() {
-		fail("Not yet implemented");
+	public void testEllipse()
+	{
+		//Pour les tests de validitée des valeurs d'entrées voir appDrawing.model.test.ShapeTest
+		
+		Ellipse e = new Ellipse(0.0f, 0.0f, 10.0f, 15.0f);
+		
+		//Vérifions si l'ellipse est créée
+		assertNotNull(e);
+		
+		//Vérifions les paramètres entrés
+		assertEquals(0.0f, e.getPosX(),0);
+		assertEquals(0.0f, e.getPosY(),0);
+		assertEquals(10.0f, e.getWidth(),0);
+		assertEquals(15.0f, e.getHeight(),0);
+		
 	}
 
 }
