a803810f970a6249e021b1a62b1ad13438a02851
micaellemelin@gmail.com <micaellemelin@gmail.com@1f91aff8-08ec-4a99-6d13-0bc85f0ddcaa>
2011-12-12 01:08:37 +0000

    Amélioration des test des poignées, commentaires ajoutés

--- trunk/Drawing/src/appDrawing/model/test/HandleTest.java
+++ trunk/Drawing/src/appDrawing/model/test/HandleTest.java
@@ -10,27 +10,36 @@ import appDrawing.model.Rectangle;
 
 public class HandleTest {
 
-	Rectangle rect = new Rectangle(0, 0, 10, 10);
+	private static final Rectangle rect = new Rectangle(0, 0, 10, 10); //Rectangle utilisé pour les tests
+	private static final float HANDLE_SIZE = 10.0f; //Taile réelle d'une poignée
 	
 	@Test
 	public void testSetSelected()
 	{
+		//On créé une poignée. Pour l'exemple on choisi une poignée en haut à gauche
 		Handle handle = new Handle(HandleType.TOP_LEFT, rect);
+		//On test la méthode qui est supposé ne pas rendre la poignée selectionnée
+		//Une poignée ne peut être selectionnée
 		handle.setSelected(true);
 		
-		assertEquals(false,handle.isSelected());//Une poignée ne peut être selectionnée
+		//On vérifie qu'elle n'est pas selectionnée
+		assertEquals(false,handle.isSelected());
 	}
 
 	@Test
 	public void testGetRealRect()
 	{
+		//On créé une poignée. Pour l'exemple on choisi une poignée en haut à gauche
 		Handle handle = new Handle(HandleType.TOP_LEFT, rect);
 		
+		//On calcule le rectangle englobant de la poignée
 		java.awt.Rectangle realRect = handle.getRealRect(1f, 0f, 0f);
 		
-		assertEquals(10, realRect.width);
-		assertEquals(10, realRect.height);
+		//La taille d'une poignée dessine un carré de taille HANDLE_SIZE
+		assertEquals(HANDLE_SIZE, realRect.width,0);
+		assertEquals(HANDLE_SIZE, realRect.height,0);
 		
+		//À la position de la poignée
 		assertEquals(rect.getPosX()-rect.getWidth()/2, realRect.x,0);
 		assertEquals(rect.getPosY()-rect.getHeight()/2, realRect.y,0);
 	}
@@ -38,7 +47,8 @@ public class HandleTest {
 	@Test
 	public void testHandle()
 	{
-		
+		//On créé plusieurs poignées de toutes les sortes
+		//Le constructeur calculera lui même les positions des poignées selon le type
 		Handle handleTL = new Handle(HandleType.TOP_LEFT, rect);
 		Handle handleTM = new Handle(HandleType.TOP_MIDDLE, rect);
 		Handle handleTR = new Handle(HandleType.TOP_RIGHT, rect);
@@ -48,35 +58,46 @@ public class HandleTest {
 		Handle handleBM = new Handle(HandleType.BOTTOM_MIDDLE, rect);
 		Handle handleBR = new Handle(HandleType.BOTTOM_RIGHT, rect);
 		
+		//Vérification de la position pour la poignée de type: TOP_LEFT
 		assertEquals(rect.getPosX(), handleTL.getPosX(),0);
 		assertEquals(rect.getPosY(), handleTL.getPosY(),0);
 		
+		//Vérification de la position pour la poignée de type: TOP_MIDDLE
 		assertEquals(rect.getWidth()/2, handleTM.getPosX(),0);
 		assertEquals(rect.getPosY(), handleTM.getPosY(),0);
 		
+		//Vérification de la position pour la poignée de type: TOP_RIGHT
 		assertEquals(rect.getWidth(), handleTR.getPosX(),0);
 		assertEquals(rect.getPosY(), handleTR.getPosY(),0);
 		
+		//Vérification de la position pour la poignée de type: MIDDLE_LEFT
 		assertEquals(rect.getPosX(), handleML.getPosX(),0);
 		assertEquals(rect.getHeight()/2, handleML.getPosY(),0);
 		
+		//Vérification de la position pour la poignée de type: MIDDLE_RIGHT
 		assertEquals(rect.getWidth(), handleMR.getPosX(),0);
 		assertEquals(rect.getHeight()/2, handleMR.getPosY(),0);
 		
+		//Vérification de la position pour la poignée de type: BOTTOM_LEFT
 		assertEquals(rect.getPosX(), handleBL.getPosX(),0);
 		assertEquals(rect.getHeight(), handleBL.getPosY(),0);
 		
+		//Vérification de la position pour la poignée de type: BOTTOM_MIDDLE
 		assertEquals(rect.getWidth()/2, handleBM.getPosX(),0);
 		assertEquals(rect.getHeight(), handleBM.getPosY(),0);
 		
+		//Vérification de la position pour la poignée de type: TOP_RIGHT
 		assertEquals(rect.getWidth(), handleBR.getPosX(),0);
 		assertEquals(rect.getHeight(), handleBR.getPosY(),0);
+		
+		//Pas besoin de vérifier les dimensions d'une poignée car celles-ci sont nulles( égales à 1.0E-9 )
 		
 	}
 
 	@Test
 	public void testGetType()
 	{
+		//On créé plusieurs poignées de toutes les sortes
 		Handle handleTL = new Handle(HandleType.TOP_LEFT, rect);
 		Handle handleTM = new Handle(HandleType.TOP_MIDDLE, rect);
 		Handle handleTR = new Handle(HandleType.TOP_RIGHT, rect);
@@ -86,6 +107,7 @@ public class HandleTest {
 		Handle handleBM = new Handle(HandleType.BOTTOM_MIDDLE, rect);
 		Handle handleBR = new Handle(HandleType.BOTTOM_RIGHT, rect);
 		
+		//Vérification que les types on bien étés attribués
 		assertEquals(HandleType.TOP_LEFT, handleTL.getType());
 		
 		assertEquals(HandleType.TOP_MIDDLE, handleTM.getType());
@@ -106,8 +128,10 @@ public class HandleTest {
 	@Test
 	public void testGetParent()
 	{
+		//On créé une poignée. Pour l'exemple on choisi une poignée en haut à gauche
 		Handle handle = new Handle(HandleType.TOP_LEFT, rect);
 		
+		//Vérification que le parent de la poignée correspond à celui entré en paramètre
 		assertSame(handle.getParent(), rect);
 	}
 
